70063b5bc44a08f68c8a0679a7ce365ce8b63e55
ninjaparade <yazjallad@gmail.com>
2015-08-03 11:03:38 -0700

    added config

---
+++ src/Pta/Flash/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	
+	'frontend' => [
+		'success' => 'success',
+		'error'   => 'alert',
+		'warning' => 'warning',
+		'info'    => 'info'
+	],
+
+	'admin' => [
+		'success' => 'success',
+		'error'   => 'alert',
+		'warning' => 'warning',
+		'info'    => 'info'
+	]
+];
